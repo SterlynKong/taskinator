ae47531b0bcdcfd334d55ac948aa7c4ee5dc9732
YoungAncient <68391309+YoungAncient@users.noreply.github.com>
2020-09-23 00:49:19 -0400

    added wireframe: form-input and prevented default page refres on click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
 var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var formEl = document.querySelector("#task-form");
 
 var createTaskHandler = function () {
+
+    event.preventDefault();
+    
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
@@ -9,4 +13,4 @@ var createTaskHandler = function () {
 }
 
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
